8d7b996a87fdd31e086dd765ca875b49917f0b54
Biziarkin Iaroslav S <comradeyar@gmail.com>
2023-09-29 08:16:59 +0300

    Changed data getting logic

--- utils/tinkoff_api.py
+++ utils/tinkoff_api.py
@@ -4,9 +4,203 @@ from . import LocalStorage
 import pandas as pd
 from datetime import datetime
 import os, json, requests as req, zipfile, gzip
+import time
 
-def get_current_year():
-  return datetime.now().year
+class TickerDataDataframe:
+  def __init__(self, path):
+    self.path = path
+    self._df = None
+
+  @property
+  def df(self):
+    if self._df is None:
+      return self.load_df_from_file()
+    return self._df
+
+  @df.setter
+  def df(self, df):
+    self._df = df
+
+  def load_df_from_file(self):
+    self._df = pd.read_csv(
+      self.path,
+      names=['date', 'time', 'open', 'close', 'high', 'low', 'volume']
+    )
+    return self.df
+
+  def update_df(self, df):
+    self.df = pd.concat([self.df, df])
+    self.df = self.df.drop_duplicates( 
+      subset=['date', 'time'], 
+      keep='last')
+    self.df = self.remove_non_trading_days()
+
+    self.delete_df_file()
+    return self.save_df_to_file()
+    
+  def delete_df_file(self):
+    os.remove(self.path)
+
+  def save_df_to_file(self, df=None):
+    if df is not None:
+      df_to_save = df
+    else:
+      df_to_save = self.df
+    df_to_save.to_csv(
+      self.path, 
+      index=False,
+      columns=['date', 'time', 'open', 'close', 'high', 'low', 'volume'],
+      header=False
+    )
+
+    return self.load_df_from_file()
+
+  @property
+  def trade_calendar(self):
+    calendar =  pd.read_csv(
+      os.path.join(
+        os.path.dirname(
+          os.path.abspath(__file__)
+        ), '../util-data/trade-calendar.csv'
+      ), names=['date', 'is_holiday'])
+
+    calendar['date'] = calendar['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').strftime('%m/%d/%Y'))
+
+    return calendar
+
+  def remove_non_trading_days(self):
+    calendar = self.trade_calendar
+    # remove non trading days
+    df = self.df[self.df['date'].isin(calendar[calendar['is_holiday'] == 0]['date'])]
+    # get types of dataframe
+    df_types = df.dtypes
+    print(df_types)
+    
+    return df
+
+  @property
+  def last_candle(self):
+    return self.df.iloc[-1]
+
+  @property
+  def last_candle_date(self):
+    return self.last_candle['date']
+
+
+
+class TickerData:
+  def __init__(self, ticker=None, ticker_type=None, figi=None):
+    self._ticker = ticker
+    self._ticker_type = ticker_type
+    self._figi = figi
+    self._df = None
+
+  def __str__(self):
+    return f'{self.ticker} {self.ticker_type} {self.figi}'
+
+  @property
+  def figi(self):
+    return self._figi
+
+  @figi.setter
+  def figi(self, figis: list[str]):
+    '''
+    Set figi from list of figis. If there is no figi with BBG in it, then set the first figi from the list
+    
+    figis: list[str]
+    
+    return: None'''
+    self._figi = None
+    for figi in figis:
+      if 'BBG' in figi:
+        self._figi = figi
+        break
+    if not self._figi:
+      self._figi = figis[0]
+  
+  @property
+  def ticker(self):
+    return self._ticker
+
+  @ticker.setter
+  def ticker(self, ticker: str):
+    self._ticker = ticker
+
+  @property
+  def ticker_type(self):
+    return self._ticker_type
+
+  @ticker_type.setter
+  def ticker_type(self, ticker_type: str):
+    self._ticker_type = ticker_type
+
+  @property
+  def file_name(self):
+    if not self.ticker or not self.figi:
+      raise Exception('Ticker and figi must be set')
+    return f'{self.ticker}_{self.figi}'
+
+  @property
+  def csv_name(self):
+    return f'{self.file_name}.csv'
+
+  @property
+  def zip_name(self):
+    return f'{self.file_name}.zip'
+
+  @property
+  def path(self):
+    if not self.ticker or not self.figi or not self.ticker_type:
+      raise Exception('Ticker, figi and ticker_type must be set')
+    if not os.path.exists(self.parent_folder_path):
+      os.mkdir(self.parent_folder_path)
+    return os.path.join(
+      LocalStorage.get_local_storage().get('folder_path'), 
+      self.ticker_type, 
+      self.csv_name)
+
+  @property
+  def parent_folder_path(self):
+    if not self.ticker_type:
+      raise Exception('ticker_type must be set')
+    return os.path.join(
+      LocalStorage.get_local_storage().get('folder_path'), 
+      self.ticker_type)
+
+  @property
+  def csv_file_exists(self):
+    return os.path.exists(self.path)
+  
+  @property
+  def df(self):
+    if not self.csv_file_exists:
+      raise Exception('csv file does not exist')
+    if not self._df:
+      self.load_df()
+    return self._df
+  
+  def load_df(self):
+    self._df = TickerDataDataframe(self.path)
+
+  @property
+  def last_candle(self):
+    return self.df.last_candle
+
+  @property
+  def last_candle_date(self):
+    return self.df.last_candle_date
+
+  @property
+  def last_candle_year(self):
+    return int(self.last_candle_date.split('/')[2])
+
+  def update_dataframe(self, df):
+    return self.df.update_df(df)
+
+  def save_new_dataframe(self, df):
+    _df = TickerDataDataframe(self.path)
+    _df.save_df_to_file(df)
+    self.load_df()
 
 class InstrumentsService:
   def __init__(self, token):
@@ -20,7 +214,8 @@ class InstrumentsService:
  
       l = []
       for method in ['shares', 'bonds', 'etfs', 'currencies', 'futures']:
-        for item in getattr(instruments, method)().instruments: #instrument_status=InstrumentStatus.INSTRUMENT_STATUS_ALL
+        for item in getattr(instruments, method)(instrument_status=InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments:
+        #for item in getattr(instruments, method)().instruments: #instrument_status=InstrumentStatus.INSTRUMENT_STATUS_ALL
           l.append({
             'ticker': item.ticker,
             'figi': item.figi,
@@ -32,68 +227,60 @@ class InstrumentsService:
 
       self.instruments = df
 
-  def find_figi_by_ticker(self, ticker, ticker_type):
-    return self.instruments[(self.instruments["ticker"] == ticker) & (self.instruments["type"] == ticker_type)].iloc[0]["figi"]
+  def find_figi_by_ticker(self, ticker_obj: TickerData):
+    # get figis by ticker and type
+    figis = self.instruments[(self.instruments["ticker"] == ticker_obj.ticker) & (self.instruments["type"] == ticker_obj.ticker_type)]["figi"].tolist()
+    if len(figis) == 0:
+      return None
+    ticker_obj.figi = figis
+    return ticker_obj
+
+  def find_ticker_by_figi(self, ticker_obj):
+    # get ticker by figi
+    tickers = self.instruments[self.instruments["figi"] == ticker_obj.figi]["ticker"].tolist()
+    if len(tickers) == 0:
+      return None
+    ticker_obj.ticker = tickers[0]
+    # get ticker type by figi
+    ticker_types =  self.instruments[self.instruments["figi"] == ticker_obj.figi]["type"].tolist()
+    if len(ticker_types) == 0:
+      return None
+    ticker_obj.ticker_type = ticker_types[0]
+    return ticker_obj
 
 class DataLoader:
   def __init__(self, token):
     self.token = token
 
-  def remove_non_trading_days(self, df):
-    # remove non trading days
-    trade_calendar = pd.read_csv(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../util-data/trade-calendar.csv'), names=['date', 'is_holiday'])
-    # convert date string from '%Y-%m-%d' to '%m/%d/%Y' string
-    trade_calendar['date'] = trade_calendar['date'].apply(lambda x: datetime.strptime(x, '%Y-%m-%d').strftime('%m/%d/%Y'))
-    # remove non trading days
-    df = df[~df['date'].isin(trade_calendar[trade_calendar['is_holiday'] == 1]['date'])]
-    return df
-
-  def load_by_ticker(self, ticker, ticker_type, instrument_service: InstrumentsService):
-    print(f'Loading {ticker} {ticker_type} data')
-    figi = instrument_service.find_figi_by_ticker(ticker, ticker_type)
-    exists = self.check_ticker_existance(ticker, ticker_type)
-    print(f'Exists: {exists}')
-    if exists:
-      
-      data = pd.read_csv(
-        self.get_ticker_path(ticker, ticker_type),
-        names=['date', 'time', 'open', 'close', 'high', 'low', 'volume']
-      )
-      # get last candle
-      last_candle = data.iloc[-1]
-      # get last candle date
-      last_candle_date = datetime.strptime(last_candle['date'], '%m/%d/%Y')
-      # get current date
-      current_date = datetime.now()
-      
-      # download data for each year from last candle date to current date inclusive
-      for year in range(last_candle_date.year, current_date.year + 1):
-        print(f'Loading {ticker} {ticker_type} data for {year}')
-        # get filename
-        filename = f'{figi}_{year}'
-        # download year data
-        df = self.download_year_data(year, figi, filename)
-        # get standard prices form
-        df = self.get_standard_prices_form(df)
-        # add unique rows to data by date and time
-        data = pd.concat([data, df]).drop_duplicates(subset=['date', 'time'], keep='last')
-      # remove ticker file
-      os.remove(self.get_ticker_path(ticker, ticker_type))
-      # save data to ticker file
-      self.remove_non_trading_days(data).to_csv(
-        self.get_ticker_path(ticker, ticker_type), 
-        index=False,
-        columns=['date', 'time', 'open', 'close', 'high', 'low', 'volume'],
-        header=False
-      )
+  def load_by_figi(self, ticker_obj: TickerData, instrument_service: InstrumentsService):
+    print(f'Load by figi {ticker_obj.figi}')
+    ticker_obj = instrument_service.find_ticker_by_figi(ticker_obj)
+    if ticker_obj is not None:
+      self.update_data(ticker_obj)
     else:
-      df = self.download_all_data(figi).drop_duplicates(subset=['date', 'time'], keep='last')
-      self.remove_non_trading_days(df).to_csv(
-        self.get_ticker_path(ticker, ticker_type), 
-        index=False,
-        columns=['date', 'time', 'open', 'close', 'high', 'low', 'volume'],
-        header=False
-      )
+      print(f'Figi not found')
+
+  def load_by_ticker(self, ticker_obj: TickerData, instrument_service: InstrumentsService):
+    print(f'Load by ticker {ticker_obj.ticker}')
+    # get figi
+    ticker_obj = instrument_service.find_figi_by_ticker(ticker_obj)
+
+    if ticker_obj is not None:
+      self.update_data(ticker_obj)
+    else:
+      print(f'Ticker not found')
+
+  def update_data(self, ticker_obj: TickerData):
+    '''
+    Update data for ticker_obj. If csv file exists, then update it, else download all data
+    
+    ticker_obj: TickerData
+    
+    return: None'''
+    if ticker_obj.csv_file_exists:
+      self.update_ticker(ticker_obj)
+    else:
+      self.download_all_data(ticker_obj)
 
   def get_standard_prices_form(self, df):
     # set moscow timezone for datetime column
@@ -107,97 +294,93 @@ class DataLoader:
     df = df.drop(columns=['datetime'])
     return df
 
-  def download_all_data(self, figi):
-    # get start year
+  def download_all_data(self, ticker_obj: TickerData):
     start_year = LocalStorage.get_config().get('start_year')
+    current_year = datetime.now().year
 
-    # get current year
-    current_year = get_current_year()
-
-    # list of datasets
     datasets = []
-    # download data for each year
+
     for year in range(start_year, current_year + 1):
-      print(f'Loading {figi} data for {year}')
-      # get filename
-      filename = f'{figi}_{year}'
-      # download year data
-      df = self.download_year_data(year, figi, filename)
-      # save data to datasets
+      df = self.download_year_data(year, ticker_obj)
       datasets.append(df)
       
-    # concat all datasets
     df = pd.concat(datasets)
 
-    # get standard prices form
     df = self.get_standard_prices_form(df)
 
-    return df
+    # if df is not empty
+    if not df.empty:
+      ticker_obj.save_new_dataframe(df)
 
-  def download_year_data(self, year, figi, filename):
-    # get zip file
+  def download_year_data(self, year, ticker_obj: TickerData):
     data = req.get(
-      f'https://invest-public-api.tinkoff.ru/history-data?figi={figi}&year={year}', 
+      f'https://invest-public-api.tinkoff.ru/history-data?figi={ticker_obj.figi}&year={year}', 
       headers={'Authorization': f'Bearer {self.token}'}
     )
-    # get buffer folder absolute path
-    buffer_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../buffer')
-    # get buffer zip file absolute path
-    buffer_zip_file_path = os.path.join(buffer_folder_path, f'{filename}.zip')
-    print()
-    # get buffer data folder
-    buffer_data_folder = os.path.join(buffer_folder_path, filename)
 
-    print(buffer_folder_path)
-    # create buffer folder if not exists
-    if not os.path.exists(buffer_folder_path):
-      os.mkdir(buffer_folder_path)
-    # create zip file
-    with open(buffer_zip_file_path, 'wb') as f:
-      f.write(data.content)
-
-    # check if file is empty
-    if os.path.getsize(buffer_zip_file_path) == 0:
+    if data.status_code == 200:
+      # get buffer folder absolute path
+      buffer_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../buffer')
+      # get buffer zip file absolute path
+      buffer_zip_file_path = os.path.join(buffer_folder_path, ticker_obj.zip_name)
+      # get buffer data folder
+      buffer_data_folder = os.path.join(buffer_folder_path, ticker_obj.file_name)
+      # create buffer folder if not exists
+      if not os.path.exists(buffer_folder_path):
+        os.mkdir(buffer_folder_path)
+      # create zip file
+      with open(buffer_zip_file_path, 'wb') as f:
+        f.write(data.content)
+      # check if file is empty
+      if os.path.getsize(buffer_zip_file_path) == 0:
+        os.remove(buffer_zip_file_path)
+        return pd.DataFrame(columns=['datetime', 'open', 'close', 'high', 'low', 'volume'])
+      # unzip file
+      with zipfile.ZipFile (buffer_zip_file_path, 'r') as zip_ref:
+        zip_ref.extractall(buffer_data_folder)
+      # remove zip file
       os.remove(buffer_zip_file_path)
+      try:
+        # read all files from folder
+        files = os.listdir(buffer_data_folder)
+      except Exception as e:
+        return pd.DataFrame(columns=['datetime', 'open', 'close', 'high', 'low', 'volume'])
+      # concat all files
+      df = pd.concat([
+        pd.read_csv(
+          os.path.join(buffer_data_folder, file), 
+          sep=';',
+          names=['datetime', 'open', 'close', 'high', 'low', 'volume'],
+          usecols=[1,2,3,4,5,6]
+        ) for file in files
+      ])
+      # remove folder even if it is not empty
+      for file in files:
+        os.remove(os.path.join(buffer_data_folder, file))
+      os.rmdir(buffer_data_folder)
+
+      return df
+    elif data.status_code == 429:
+      print('Too many requests')
+      time.sleep(5)
+      return self.download_year_data(year, ticker_obj)
+    elif data.status_code == 401:
+      print('Unauthorized')
+      raise Exception('Unauthorized')
+    elif data.status_code == 404:
+      print(f'Not found: {ticker_obj.figi} {year}')
       return pd.DataFrame(columns=['datetime', 'open', 'close', 'high', 'low', 'volume'])
-    # unzip file
-    with zipfile.ZipFile (buffer_zip_file_path, 'r') as zip_ref:
-      zip_ref.extractall(buffer_data_folder)
-
-    # remove zip file
-    os.remove(buffer_zip_file_path)
-
-    # read all files from folder
-    files = os.listdir(buffer_data_folder)
-
-    # concat all files
-    df = pd.concat([
-      pd.read_csv(
-        os.path.join(buffer_data_folder, file), 
-        sep=';',
-        names=['datetime', 'open', 'close', 'high', 'low', 'volume'],
-        usecols=[1,2,3,4,5,6]
-      ) for file in files
-    ])
-    print(df.head(5))
-
-    # remove folder even if it is not empty
-    for file in files:
-      os.remove(os.path.join(buffer_data_folder, file))
-    os.rmdir(buffer_data_folder)
-
-    return df
-
-  def get_ticker_path(self, ticker, ticker_type):
-    # create ticker_type folder if not exists
-    if not os.path.exists(os.path.join(LocalStorage.get_local_storage().get('folder_path'), ticker_type)):
-      os.mkdir(os.path.join(LocalStorage.get_local_storage().get('folder_path'), ticker_type))
-    # return ticker path
-    return os.path.join(LocalStorage.get_local_storage().get('folder_path'), ticker_type, f'{ticker}.csv')
-
-  def check_ticker_existance(self, ticker, ticker_type):
-    data_path = self.get_ticker_path(ticker, ticker_type)
-    return os.path.exists(data_path) 
+    else:
+      print('Unknown error')
+      raise Exception('Unknown error')
+    
+  def update_ticker(self, ticker_obj: TickerData):    
+    # download data for each year from last candle date to current date inclusive
+    for year in range(ticker_obj.last_candle_year, datetime.now().year + 1):
+      df = self.download_year_data(year, ticker_obj)
+      df = self.get_standard_prices_form(df)
+      ticker_obj.update_dataframe(df)
+    
 
 class TinkoffApi:
   def __init__(self):
@@ -209,6 +392,7 @@ class TinkoffApi:
         print(e)
         self.instruments_service = None
         self.token = None
+        raise e
     else:
       self.instruments_service = None
   
@@ -220,5 +404,10 @@ class TinkoffApi:
       self.instruments_service = None
 
   def update_prices_by_ticker(self, ticker, ticker_type):
-    DataLoader(self.token).load_by_ticker(ticker, ticker_type, self.instruments_service)
+    ticker_obj = TickerData(ticker=ticker, ticker_type=ticker_type)
+    DataLoader(self.token).load_by_ticker(ticker_obj, self.instruments_service)
+
+  def update_prices_by_figi(self, figi):
+    ticker_obj = TickerData(figi=figi)
+    DataLoader(self.token).load_by_figi(ticker_obj, self.instruments_service)
   
